9ab04a37c8687ed9e12511bfa60ceac945c7a206
Sylvain CUGNOD <sylvain.cugnod@gmail.com>
2016-03-18 10:27:01 +0100

    Inté Annonces active - Ptit cadeau

--- resources/views/dashboard/trip_offers/active.blade.php
+++ resources/views/dashboard/trip_offers/active.blade.php
@@ -22,7 +22,7 @@
     <div class="col m9 s12">
         <div class="card">
             <div class="card-content">
-                <h3>Mes annonces</h3>
+                <h2>Mes annonces</h2>
 
                 @if(Auth::user()->site_id == null)
                 <p class="alert alert-warning">Vous devez ajouter un site de formation avant de pouvoir créer une annonce !</p>
@@ -32,39 +32,47 @@
                 <div>Vous devez <a href="{{ url('/profile/car/create') }}" class="link">ajouter un véhicule</a> avant de pouvoir créer une annonce !</div>
                 @endif
 
-                <div>
-                    @forelse($trajets as $trajet)
-                        <div class="row">
-                            <div class="card card-giddy col l6 s12">
-                                <div class="card-content">
-                                    <div class="row">
-                                        <div class="col s12 center-align">
-                                            <p class="card-title ">{{ $trajet->depart }} <i class="material-icons">arrow_forward</i> {{ $trajet->arrivee }}</p>
-                                            <p>{{ date('d F Y',strtotime($trajet->trajet_date)) }} à {{ date('H:i',strtotime($trajet->trajet_heure)) }}</p>
-                                            <p>{{ $trajet->marque }} {{ $trajet->modele }}</p>
-                                            <p>{{ $trajet->trajet_place }} places</p>
-                                            <p>{{ $trajet->trajet_tarif }}€ par passager</p>
-
-                                            <a href="{{ url('trip-offers/'.$trajet->trajet_id) }}" class="waves-effect waves-light btn"><i class="material-icons left">remove_red_eye</i>Voir l'annonce</a>
-
-                                            <a class="waves-effect waves-light btn"><i class="material-icons left">airline_seat_recline_normal</i>Voir vos passagers</a>
-
-                                            <a class="waves-effect waves-light btn"><i class="material-icons left">settings</i>Modifier</a>
-
-                                            <a class="waves-effect waves-light btn"><i class="material-icons left">clear</i>Supprimer</a>
-
-
-                                        </div>
+                @forelse($trajets as $trajet)
+                <div class="row">
+                    <div class="card card-giddy col l6 m12">
+                        <div class="card-content row">
+                            <div class="col l6 s12">
+                                <h4>{{ $trajet->depart }} <i class="material-icons icon-valign">arrow_forward</i> {{ $trajet->arrivee }}</h4>
+                                <h5>{{ date('d F Y',strtotime($trajet->trajet_date)) }} à {{ date('H:i',strtotime($trajet->trajet_heure)) }}</h5>
+                                <div class="card card-info col s6">
+                                    <div class="card-content center-align">
+                                        <i class="material-icons">airline_seat_recline_normal</i><br>
+                                        {{ $trajet->trajet_place }} places
+                                    </div>
+                                </div>
+                                <div class="card card-info col s6">
+                                    <div class="card-content center-align">
+                                        <i class="material-icons">euro_symbol</i><br>
+                                        {{ $trajet->trajet_tarif }}€
+                                    </div>
+                                </div>
+                                <div class="card card-info col s12">
+                                    <div class="card-content center-align">
+                                        <i class="material-icons">directions_car</i><br>
+                                        {{ $trajet->marque }}<br>{{ $trajet->modele }}
                                     </div>
                                 </div>
                             </div>
+                            <div class="col l6 s12">
+                                <a class="waves-effect waves-light btn btn-secondary col s12"><i class="material-icons left">remove_red_eye</i>Voir l'annonce</a>
+                                <a class="waves-effect waves-light btn btn-secondary col s12"><i class="material-icons left">airline_seat_recline_normal</i>Passagers</a>
+                                <a class="waves-effect waves-light btn btn-secondary col s12"><i class="material-icons left">settings</i>Modifier</a>
+                                <a class="waves-effect waves-light btn btn-third col s12"><i class="material-icons left">clear</i>Supprimer</a>
+                            </div>
                         </div>
-                    @empty
-                    <p>Vous n'avez aucune annonce.</p>
-                    @endforelse
+                    </div>
                 </div>
             </div>
+            @empty
+            <p class="alert alert-info">Vous n'avez aucune annonce.</p>
+            @endforelse
         </div>
     </div>
+</div>
 </div>
 @endsection
